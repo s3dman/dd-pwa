8b17d7fd698bd831358b3d17655b2af6dc7effd3
NevilleJS <69287021+s3dman@users.noreply.github.com>
2023-08-28 22:53:47 +0530

    bruh

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,14 +2,10 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div className="card">
-        <button onClick={() => setCount(Math.floor(Math.random() * 100))}>
-          Nevin is {count}% gay
-        </button>
+      <div>
+        <h1>NIGGA STFU</h1>
       </div>
     </>
   );
