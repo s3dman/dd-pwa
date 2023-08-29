783380c478c2a4745d876ecbaa62c8b5190ec76a
NevilleJS <69287021+s3dman@users.noreply.github.com>
2023-08-29 13:02:57 +0530

    rbuh

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,27 @@
 import { useState } from "react";
-import "./App.css";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <>
       <div>
-        <h1>NIGGA STFU</h1>
+        <h1>nevin is gay</h1>
+        <div className="content">
+          <p>
+            Lorem ipsum dolor sit amet, officia excepteur ex fugiat
+            reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit
+            ex esse exercitation amet. Nisi anim cupidatat excepteur officia.
+            Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate
+            voluptate dolor minim nulla est proident. Nostrud officia pariatur
+            ut officia. Sit irure elit esse ea nulla sunt ex occaecat
+            reprehenderit commodo officia dolor Lorem duis laboris cupidatat
+            officia voluptate. Culpa proident adipisicing id nulla nisi laboris
+            ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo
+            ex non excepteur duis sunt velit enim. Voluptate laboris sint
+            cupidatat ullamco ut ea consectetur et est culpa et culpa duis.
+          </p>
+        </div>
+        <Footer />
       </div>
     </>
   );
---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div>
+        <p>this is the footer</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
