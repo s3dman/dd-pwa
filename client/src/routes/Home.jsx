1935435b898b960385c892d00bbb35283643b81f
NevilleJS <69287021+s3dman@users.noreply.github.com>
2023-10-18 09:32:00 +0530

    fixed homepage mostly

--- client/src/routes/Home.jsx
+++ client/src/routes/Home.jsx
@@ -26,27 +26,39 @@ const Home = () => {
         It is always the result of high intentions, sincere efforts and
         <br />
         skillful execution in search of Excellence...
-        <h2>Expertise</h2>
+        <div className="expertise-title">
+          <h2>Expertise</h2>
+        </div>
         <div className="expertise-container">
           <div className="expertise-img-container">
             <img src={img11} alt="" className="expertise-img" />
             <div className="expertise-overlay">
               <h3>ARCHITECTURE</h3>
-              <p>Expertise in architectural design and planning.</p>
+              <p className="overlay-desc">
+                We are passionate about responsible design and the impact it has
+                on the lives we touch every day.
+              </p>
             </div>
           </div>
           <div className="expertise-img-container">
             <img src={img12} alt="" className="expertise-img" />
             <div className="expertise-overlay">
               <h3>INTERIOR DESIGN</h3>
-              <p>Expertise in interior design and decoration.</p>
+              <p className="overlay-desc">
+                We create functional interior design solutions which address the
+                needs & requirements of our client.
+              </p>
             </div>
           </div>
           <div className="expertise-img-container">
             <img src={img13} alt="" className="expertise-img" />
             <div className="expertise-overlay">
               <h3>PLANNING</h3>
-              <p>Expertise in urban planning and development.</p>
+              <p className="overlay-desc">
+                We are committed to delivering environmental and socially
+                sustainable communities with a strong sense of design value on
+                an urban level.
+              </p>
             </div>
           </div>
         </div>
