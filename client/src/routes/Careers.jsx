0d3beefb02576ab78b91ca72692434873156e43c
n3v1nnn <144786610+n3v1nnn@users.noreply.github.com>
2023-10-20 10:24:44 +0530

    changes career

--- client/src/routes/Careers.jsx
+++ client/src/routes/Careers.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 import "../styles/Careers.css";
+import img10 from "../assets/main_logo_black.png";
+
+const s = [
+  { src: img10, title: "BLACK LOGO" },
+  { src: img4, title: "NAVACHETANA HOSPITAL" },
+  { src: img5, title: "CASAGRAND ORLENA" },
+];
 
 function Careers() {
   return (
     <>
-      hello
-    </>
+    <Slider data={s} />
+    <div className="careers">
+      <img src={img10} alt="LOGO" className="BLACK_LOGO" />
+    </div>
+  </>
   );
 
 }
