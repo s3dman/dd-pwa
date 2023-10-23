e069c899d83f175cda3996104e8062aef100c998
n3v1nnn <144786610+n3v1nnn@users.noreply.github.com>
2023-10-23 07:40:55 +0530

    career page

--- client/src/routes/Careers.jsx
+++ client/src/routes/Careers.jsx
@@ -1,21 +1,24 @@
 import React from "react";
 import "../styles/Careers.css";
+import "../styles/global.css";
 import img10 from "../assets/main_logo_black.png";
 
-const s = [
-  { src: img10, title: "BLACK LOGO" },
-  { src: img4, title: "NAVACHETANA HOSPITAL" },
-  { src: img5, title: "CASAGRAND ORLENA" },
-];
+
 
 function Careers() {
   return (
     <>
-    <Slider data={s} />
-    <div className="careers">
+    <div className="logo">
       <img src={img10} alt="LOGO" className="BLACK_LOGO" />
     </div>
-  </>
+    <div id="careers-heading">
+      <h1>Careers</h1>
+    </div>
+    <div id="career-paragraph">
+      <h3>Our studio has grown to become more than just a place of work to our staff members. It is a place where people can achieve great things, invest in themselves<br/> and be rewarded for it. This has contributed to a phenomenal team that is strong and able and this sort of functionality gives us the freedom to focus on our<br/> core business practice and what we do best. It has allowed us to serve our clients to the best of our ability, achieving their satisfaction, and not stopping until<br/> we have done so.</h3>
+    </div>
+
+    </>
   );
 
 }
