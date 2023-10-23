ba6fc20fe4c9860abf401b6a620e0b44dda47763
thearfa99 <132257607+thearfa99@users.noreply.github.com>
2023-10-24 00:02:35 +0530

    Left side updated [contact page]
    lot of css in px need to convert to relative spacing

--- client/src/routes/Contact.jsx
+++ client/src/routes/Contact.jsx
@@ -1,12 +1,32 @@
 import React from "react";
+import "../styles/Contact.css";
 
-function Contact() {
+const Contact = () => {
   return (
-    <>
-      <h1>Contact</h1>
-      <p>please dont contact me</p>
-    </>
+    <div className="two-column-container">
+      <div className="column left-column">
+        {<h5>Contact</h5>}
+        <div className="Hours">
+        {<h2>Hours</h2>}
+        {<p>Monday - Friday : 9 AM to 6 PM</p>}
+        {<p>Saturday : 9 AM to 1 PM</p>}
+        {<p>Sunday Closed</p>}
+        </div>
+        <div className="Enquires">
+        {<h2>Enquires</h2>}
+        {<p>info@destinationdesigns.net.in</p>}
+        </div>
+        <div className="WWS">
+        {<h2>Work with us</h2>}
+        {<p>To submit your Resume please check our careers page.</p>}
+        {<p>Careers</p>}
+        </div>
+      </div>
+      <div className="column right-column">
+        {"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}
+      </div>
+    </div>
   );
-}
+};
 
 export default Contact;
