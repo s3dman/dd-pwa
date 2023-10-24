15dc7c24801431294c6f32a04ce819bd249ea932
n3v1nnn <144786610+n3v1nnn@users.noreply.github.com>
2023-10-24 09:40:33 +0530

    career changes

--- client/src/routes/Careers.jsx
+++ client/src/routes/Careers.jsx
@@ -6,7 +6,6 @@ import img10 from "../assets/main_logo_black.png";
 function Careers() {
   return (
     <>
-<<<<<<< Updated upstream
       <div className="logo">
         <img src={img10} alt="LOGO" className="BLACK_LOGO" />
       </div>
@@ -14,64 +13,65 @@ function Careers() {
         <h1>Careers</h1>
       </div>
       <div id="career-paragraph">
-        <h3>
+        <p>
           Our studio has grown to become more than just a place of work to our
           staff members. It is a place where people can achieve great things,
           invest in themselves
-          <br /> and be rewarded for it. This has contributed to a phenomenal
-          team that is strong and able and this sort of functionality gives us
-          the freedom to focus on our
-          <br /> core business practice and what we do best. It has allowed us
-          to serve our clients to the best of our ability, achieving their
-          satisfaction, and not stopping until
-          <br /> we have done so.
-        </h3>
-      </div>
-=======
-    <div className="logo">
-      <img src={img10} alt="LOGO" className="BLACK_LOGO" />
-    </div>
-    <div id="careers-heading">
-      <h1>Careers</h1>
-    </div>
-    <div id="career-paragraph">
-      <p>Our studio has grown to become more than just a place of work to our staff members. It is a place where people can achieve great things, invest in themselves<br/> 
-          and be rewarded for it. This has contributed to a phenomenal team that is strong and able and this sort of functionality gives us the freedom to focus on our<br/>
-          core business practice and what we do best. It has allowed us to serve our clients to the best of our ability, achieving their satisfaction, and not stopping until<br/>
+          <br />
+          and be rewarded for it. This has contributed to a phenomenal team that
+          is strong and able and this sort of functionality gives us the freedom
+          to focus on our
+          <br />
+          core business practice and what we do best. It has allowed us to serve
+          our clients to the best of our ability, achieving their satisfaction,
+          and not stopping until
+          <br />
           we have done so.
-      </p> 
-    </div>
+        </p>
+      </div>
 
 
 
-    <div id="elements2" classname="flex-container">
-    <div id="careers-featured-roles" >
-      <h2>Featured Roles</h2>
-      <p id="careers-roles">
-      ARCHITECTURAL ASSISTANT<br/>
-      JUNIOR ARCHITECT<br/>
-      ASSISTANT ARCHITECT<br/>
-      ARCHITECT<br/>
-      SENIOR ARCHITECT<br/><br/><br/>
-      For Internship please mail us your portfolio to<br/>
-      info@destinationdesigns.net.in 
-      </p>
-    </div>
 
-    <div>
-                <form id="careers-form">
-                    <input type="text" placeholder="First Name"/>
-                    <input type="text" placeholder="Last Name"/><br/><br/>
-                    <input type="text" placeholder="Your Email"/><br/><br/>
-                    <input type="text" placeholder="Applying Position"/><br/><br/>
-                    <input type="button" placeholder="Choose a file..."/>
-        
-        
-        
-                </form></div>
-    </div>
 
->>>>>>> Stashed changes
+      <div id="elements2" classname="flex-container">
+        <div id="careers-featured-roles">
+          <h2>Featured Roles</h2>
+          <p id="careers-roles">
+            ARCHITECTURAL ASSISTANT
+            <br />
+            JUNIOR ARCHITECT
+            <br />
+            ASSISTANT ARCHITECT
+            <br />
+            ARCHITECT
+            <br />
+            SENIOR ARCHITECT
+            <br />
+            <br />
+            <br />
+            For Internship please mail us your portfolio to
+            <br />
+            info@destinationdesigns.net.in
+          </p>
+        </div>
+
+        <div id="careers-form">
+          <form >
+            <input type="text" placeholder="First Name" />
+            <input type="text" placeholder="Last Name" />
+            <br />
+            <br />
+            <input type="text" placeholder="Your Email" />
+            <br />
+            <br />
+            <input type="text" placeholder="Applying Position" />
+            <br />
+            <br />
+            <input type="button" placeholder="Choose a file..." />
+          </form>
+        </div>
+      </div>
     </>
   );
 }
