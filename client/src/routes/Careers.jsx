6231bcdd79069f2a2681b621c03e57cb0263de2b
n3v1nnn <144786610+n3v1nnn@users.noreply.github.com>
2023-10-25 23:44:34 +0530

    career

--- client/src/routes/Careers.jsx
+++ client/src/routes/Careers.jsx
@@ -6,70 +6,137 @@ import img10 from "../assets/main_logo_black.png";
 function Careers() {
   return (
     <>
-      <div className="logo">
-        <img src={img10} alt="LOGO" className="BLACK_LOGO" />
-      </div>
-      <div id="careers-heading">
-        <h1>Careers</h1>
-      </div>
-      <div id="career-paragraph">
-        <p>
-          Our studio has grown to become more than just a place of work to our
-          staff members. It is a place where people can achieve great things,
-          invest in themselves
-          <br />
-          and be rewarded for it. This has contributed to a phenomenal team that
-          is strong and able and this sort of functionality gives us the freedom
-          to focus on our
-          <br />
-          core business practice and what we do best. It has allowed us to serve
-          our clients to the best of our ability, achieving their satisfaction,
-          and not stopping until
-          <br />
-          we have done so.
-        </p>
-      </div>
-
-
-
-
-
-      <div id="elements2" classname="flex-container">
-        <div id="careers-featured-roles">
-          <h2>Featured Roles</h2>
-          <p id="careers-roles">
-            ARCHITECTURAL ASSISTANT
-            <br />
-            JUNIOR ARCHITECT
-            <br />
-            ASSISTANT ARCHITECT
-            <br />
-            ARCHITECT
-            <br />
-            SENIOR ARCHITECT
-            <br />
-            <br />
-            <br />
-            For Internship please mail us your portfolio to
-            <br />
-            info@destinationdesigns.net.in
-          </p>
+      <div id="entire-career">
+        <div className="logo">
+          <img src={img10} alt="LOGO" className="BLACK_LOGO" />
         </div>
+        <div id="career-body">
+          <div id="careers-heading">
+            <h1>Careers</h1>
+          </div>
+          <div id="career-paragraph">
+            <p>
+              Our studio has grown to become more than just a place of work to
+              our staff members. It is a place where people can achieve great
+              things, invest in themselves
+              <br />
+              and be rewarded for it. This has contributed to a phenomenal team
+              that is strong and able and this sort of functionality gives us
+              the freedom to focus on our
+              <br />
+              core business practice and what we do best. It has allowed us to
+              serve our clients to the best of our ability, achieving their
+              satisfaction, and not stopping until
+              <br />
+              we have done so.
+            </p>
+          </div>
 
-        <div id="careers-form">
-          <form >
-            <input type="text" placeholder="First Name" />
-            <input type="text" placeholder="Last Name" />
-            <br />
-            <br />
-            <input type="text" placeholder="Your Email" />
-            <br />
-            <br />
-            <input type="text" placeholder="Applying Position" />
-            <br />
-            <br />
-            <input type="button" placeholder="Choose a file..." />
-          </form>
+          <div className="flex-container-form">
+            <div className="flex-item" id="careers-featured-roles">
+              <h2>Featured Roles</h2>
+              <p id="careers-roles">
+                ARCHITECTURAL ASSISTANT
+                <br />
+                JUNIOR ARCHITECT
+                <br />
+                ASSISTANT ARCHITECT
+                <br />
+                ARCHITECT
+                <br />
+                SENIOR ARCHITECT
+                <br />
+                <br />
+                <br />
+                For Internship please mail us your portfolio to
+                <br />
+                info@destinationdesigns.net.in
+              </p>
+            </div>
+
+            <div className="flex-item" id="careers-form">
+              <form>
+                <table id="careers-table">
+                  <tr>
+                    <td>
+                      <input
+                        id="first-name-form"
+                        type="text"
+                        className="form-textbox"
+                        placeholder="First Name"
+                      />
+                    </td>
+                    <td>
+                      <input
+                        id="second-name-form"
+                        type="text"
+                        className="form-textbox"
+                        placeholder="Last Name"
+                      />
+                    </td>
+                  </tr>
+                  <tr>
+                    <th colspan="2">
+                      <input
+                        type="text"
+                        size="50"
+                        className="form-textbox"
+                        placeholder="Your Email"
+                      />
+                    </th>
+                  </tr>
+                  <tr>
+                    <td colspan="2">
+                      <select
+                        value="Applying Position"
+                        className="form-textbox"
+                      >
+                        <option value="" disabled selected>
+                          Select an option
+                        </option>
+                        <option value="ARCHITECTURAL ASSISTANT">
+                          ARCHITECTURAL ASSISTANT
+                        </option>
+                        <option value="JUNIOR ARCHITECT">
+                          JUNIOR ARCHITECT
+                        </option>
+                        <option value="ASSISTANT ARCHITECT">
+                          ASSISTANT ARCHITECT
+                        </option>
+                        <option value="ARCHITECT">ARCHITECT</option>
+                        <option value="SENIOR ARCHITECT">
+                          SENIOR ARCHITECT
+                        </option>
+                      </select>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td className="form-above-button">Attach your Resume</td>
+                    <td className="form-above-button">Attach your Portfolio</td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <input
+                        type="button"
+                        className="form-choose-a-file"
+                        value="Choose a file..."
+                      />
+                    </td>
+                    <td>
+                      <input
+                        type="button"
+                        className="form-choose-a-file"
+                        value="Choose a file..."
+                      />
+                    </td>
+                  </tr>
+                  <tr>
+                    <input type="button" className="form-apply" value="Apply" />
+                  </tr>
+                </table>
+              </form>
+            </div>
+          </div>
         </div>
       </div>
     </>
