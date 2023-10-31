17ba8962a30002b6cb359aa4ea7241240cff05c1
NevilleJS <69287021+s3dman@users.noreply.github.com>
2023-10-31 12:49:45 +0530

    bruh

--- client/src/routes/Projects.jsx
+++ client/src/routes/Projects.jsx
@@ -1,19 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import fetchImages from "../components/IBP";
 
 function Projects() {
+  const [images, setImages] = useState([]);
+  useEffect(() => {
+    fetchImages("http://localhost:3000/image-bundle/sliders", true)
+      .then((fetchedImages) => {
+        setImages(fetchedImages);
+      })
+      .catch((error) => {
+        console.error("Failed to fetch and process images:", error);
+      });
+  }, []);
+
   return (
-    <>
-      <h1>Projects</h1>
-      <p>
-        It had been a simple realization that had changed Debra's life
-        perspective. It was really so simple that she was embarrassed that she
-        had lived the previous five years with the way she measured her worth.
-        Now that she saw what she had been doing, she could see how sad it was.
-        That made her all the more relieved she had made the change. The number
-        of hearts her Instagram posts received wasn't any longer the indication
-        of her own self-worth.
-      </p>
-    </>
+    <div>
+      {images.map((img, index) => (
+        <img key={index} src={img.src} alt={`Image ${index}`} />
+      ))}
+    </div>
   );
 }
 
