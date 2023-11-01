4a610ef77e3dff48e7151594f92c2de47e94d5bb
NevilleJS <69287021+s3dman@users.noreply.github.com>
2023-11-02 00:55:57 +0530

    projects page kinda working

--- client/src/routes/Projects.jsx
+++ client/src/routes/Projects.jsx
@@ -1,12 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ImgFetch from "../components/ImgFetch/ImgFetch";
 import "../styles/Projects.css";
 
 function Projects() {
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch(
+      "http://localhost:3000/project/ARCHITECTURE/residential/CASAGRAND ORLENA",
+    )
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("Project not found");
+        }
+      })
+      .then((data) => {
+        setImages(data);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        setError(error.message);
+        setIsLoading(false);
+      });
+  }, []);
+
   return (
-    <div className="main">
-      <ImgFetch src="SLIDERS/SHEET 6.jpg" />
-    </div>
+    <>
+      {isLoading && <p>Loading...</p>}
+      {error && <p>Error: {error}</p>}
+      <div className="image-grid">
+        {images.map((image, index) => (
+          <ImgFetch
+            key={index}
+            src={`projects/ARCHITECTURE/residential/CASAGRAND ORLENA/${image}`}
+          />
+        ))}
+      </div>
+    </>
   );
 }
 
