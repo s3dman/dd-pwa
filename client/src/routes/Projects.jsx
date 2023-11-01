a780c0caeeec1663728c81c447675b0c2a0e32ef
NevilleJS <69287021+s3dman@users.noreply.github.com>
2023-11-02 01:56:47 +0530

    kinda did projects

--- client/src/routes/Projects.jsx
+++ client/src/routes/Projects.jsx
@@ -1,46 +1,57 @@
 import React, { useState, useEffect } from "react";
-import ImgFetch from "../components/ImgFetch/ImgFetch";
+import { Link } from "react-router-dom";
 import "../styles/Projects.css";
 
 function Projects() {
-  const [images, setImages] = useState([]);
+  const [projectData, setProjectData] = useState({});
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    fetch(
-      "http://localhost:3000/project/ARCHITECTURE/residential/CASAGRAND ORLENA",
-    )
+    fetch("http://localhost:3000/projects")
       .then((response) => {
         if (response.ok) {
           return response.json();
         } else {
-          throw new Error("Project not found");
+          throw new Error("Failed to fetch projects");
         }
       })
       .then((data) => {
-        setImages(data);
+        setProjectData(data);
         setIsLoading(false);
       })
-      .catch((error) => {
-        setError(error.message);
+      .catch((err) => {
+        setError(err);
         setIsLoading(false);
       });
   }, []);
 
   return (
-    <>
-      {isLoading && <p>Loading...</p>}
-      {error && <p>Error: {error}</p>}
-      <div className="image-grid">
-        {images.map((image, index) => (
-          <ImgFetch
-            key={index}
-            src={`projects/ARCHITECTURE/residential/CASAGRAND ORLENA/${image}`}
-          />
-        ))}
-      </div>
-    </>
+    <div className="container">
+      {isLoading && <p>Loading projects...</p>}
+      {error && <p>Error: {error.message}</p>}
+      {Object.entries(projectData).map(([classKey, classData]) => (
+        <ul key={classKey}>
+          <h3 className="class-heading">{classKey}</h3>
+          <ul>
+            {Object.entries(classData).map(([typeKey, projects]) => (
+              <li key={typeKey}>
+                <h4 className="type-heading">{typeKey}</h4>
+                <ol>
+                  {projects.map((project) => (
+                    <li key={project}>
+                      <Link to={`/project/${project}`} className="project-link">
+                        {project}
+                      </Link>
+                    </li>
+                  ))}
+                </ol>
+              </li>
+            ))}
+          </ul>
+        </ul>
+      ))}
+    </div>
   );
 }
 
