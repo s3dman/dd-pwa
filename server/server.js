0c9030cc255990969e3df6620905d5be4aa2b8d1
NevilleJS <69287021+s3dman@users.noreply.github.com>
2023-11-06 14:10:59 +0530

    bruh

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const path = require("path");
 const fs = require("fs");
 
 const app = express();
+app.use(cors());
 const port = process.env.PORT || 3000;
 
 function getFoldersRecursively(dir) {
